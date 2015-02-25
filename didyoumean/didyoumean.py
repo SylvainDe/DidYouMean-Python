e37691a139ccb9102200910f3df866c0eb6cad5b
Sylvain <sylvain.desodt+github@gmail.com>
2015-02-25 20:58:04 +0000

    Reorganise code, split into small functions, add documentations

--- didyoumean/didyoumean.py
+++ didyoumean/didyoumean.py
@@ -10,6 +10,10 @@ STAND_MODULES = set(['string', 'os', 'sys', 're', 'math', 'random',
                      'datetime', 'timeit', 'unittest', 'itertools',
                      'functools'])
 
+#: Almost synonyms methods that can be confused from one type to another
+# To be completed
+SYNONYMS_SETS = [set(['add', 'append']), set(['extend', 'update'])]
+
 
 def merge_dict(*dicts):
     """Merge dictionnaries.
@@ -36,58 +40,110 @@ def get_close_matches(word, possibilities):
     return difflib.get_close_matches(word, possibilities)
 
 
-def get_var_suggestions(var, frame):
-    """Get the suggestions closest to the variable names."""
-    sugg = []
+def suggest_name_as_attribute(name, objdict):
+    """Suggest that name could be an attribute of an object.
+    Example: 'do_stuff()' -> 'self.do_stuff()'."""
+    return [nameobj + '.' + name
+            for nameobj, obj in objdict.items()
+            if hasattr(obj, name)]
+
+
+def suggest_name_as_standard_module(name):
+    """Suggest that name could be a non-importer standard module.
+    Example: 'os.whatever' -> 'import os' and then 'os.whatever'."""
+    return ['import ' + name] if name in STAND_MODULES else []
+
+
+def suggest_name_as_name_typo(name, objdict):
+    """Suggest that name could be a typo (misspelled existing name).
+    Example: 'foobaf' -> 'foobar'."""
+    return get_close_matches(name, objdict.keys())
+
+
+def suggest_name_as_keyword_typo(name):
+    """Suggest that name could be a typo (misspelled keyword).
+    Example: 'yieldd' -> 'yield'."""
+    return get_close_matches(name, keyword.kwlist)
+
+
+def get_name_suggestions(name, frame):
+    """Get the suggestions for name in case of NameError."""
     objs = get_objects_in_frame(frame)
-    for name, obj in objs.items():
-        if hasattr(obj, var):
-            sugg.append(name + '.' + var)
-    if var in STAND_MODULES:
-        sugg.append('import ' + var)
-    sugg.extend(get_close_matches(
-        var,
-        list(objs.keys()) + keyword.kwlist))
-    return sugg
+    return suggest_name_as_attribute(name, objs) + \
+        suggest_name_as_standard_module(name) + \
+        suggest_name_as_name_typo(name, objs) + \
+        suggest_name_as_keyword_typo(name)
 
 
-def get_method_suggestions(type_str, method, frame):
-    """Get the suggestions closest to the method name for a given type."""
-    sugg = []
-    if method in frame.f_builtins:
-        sugg.append(method + '(' + type_str + ')')
+def suggest_attribute_as_builtin(attribute, type_str, frame):
+    """Suggest that a builtin was used as an attribute.
+    Example: 'lst.len()' -> 'len(lst)'."""
+    return [attribute + '(' + type_str + ')'] \
+        if attribute in frame.f_builtins else []
+
+
+def suggest_attribute_synonyms(attribute, attributes):
+    """Suggest that a method with a similar meaning was used.
+    Example: 'lst.add(e)' -> 'lst.append(e)'."""
+    return [syn
+            for set_sub in SYNONYMS_SETS
+            if attribute in set_sub
+            for syn in set_sub & attributes]
+
+
+def suggest_attribute_as_typo(attribute, attributes):
+    """Suggest the attribute could be a typo.
+    Example: 'a.do_baf()' -> 'a.do_bar()'."""
+    return get_close_matches(attribute, attributes)
+
+
+def get_attribute_suggestions(type_str, attribute, frame):
+    """Get the suggestions closest to the attribute name for a given type."""
     # For module, we want to get the actual name of the module
-    if type_str == 'module':
-        type_str = frame.f_code.co_names[0]  # this probably works
-    attributes = set(dir(get_objects_in_frame(frame)[type_str]))
+    module_name = frame.f_code.co_names[0]
+    type_or_module = module_name if type_str == 'module' else type_str
+    attributes = set(dir(get_objects_in_frame(frame)[type_or_module]))
 
-    # Logic for usual containers : add <-> append, etc
-    for set_sub in [set(['add', 'append']), set(['extend', 'update'])]:
-        if method in set_sub:
-            sugg.extend(set_sub & attributes)
-
-    sugg.extend(get_close_matches(
-        method,
-        attributes))
-    return sugg
+    return suggest_attribute_as_builtin(attribute, type_str, frame) + \
+        suggest_attribute_synonyms(attribute, attributes) + \
+        suggest_attribute_as_typo(attribute, attributes)
 
 
 def import_from_frame(module_name, frame):
     """Wrapper around import to use information from frame."""
-    return __import__(module_name, globals=frame.f_globals, locals=frame.f_locals)
+    return __import__(
+        module_name,
+        globals=frame.f_globals,
+        locals=frame.f_locals)
+
+
+def suggest_imported_name_as_typo(imported_fail, frame):
+    """Suggest that imported name could be a typo from actual name in module.
+    Example: 'from math import pie' -> 'from math import pi'."""
+    module_name = frame.f_code.co_names[0]
+    return get_close_matches(
+        imported_fail,
+        dir(import_from_frame(module_name, frame)))
+
+
+def suggest_import_from_standard_module(imported_fail, frame):
+    """Suggest than name could be found in a standard module.
+    Example: 'from itertools import pi' -> 'from math import pi'."""
+    return ['from %s import %s' % (mod, imported_fail)
+            for mod in STAND_MODULES
+            if imported_fail in dir(import_from_frame(mod, frame))]
 
 
 def get_imported_suggestion(imported_fail, frame):
     """Get the suggestions closest to the failing import."""
-    sugg = []
-    module_name = frame.f_code.co_names[0]
-    sugg.extend(get_close_matches(
-        imported_fail,
-        dir(import_from_frame(module_name, frame))))
-    for mod in STAND_MODULES:
-        if imported_fail in dir(import_from_frame(mod, frame)):
-            sugg.append('from %s import %s' % (mod, imported_fail))
-    return sugg
+    return suggest_imported_name_as_typo(imported_fail, frame) + \
+        suggest_import_from_standard_module(imported_fail, frame)
+
+
+def get_module_name_suggestion(module_str):
+    """Get the suggestions closest to the failing module import.
+    Example: 'import maths' -> 'import math'."""
+    return get_close_matches(module_str, STAND_MODULES)
 
 
 def get_suggestion_string(sugg):
@@ -121,44 +177,48 @@ def add_suggestions_to_exception(type_, value, traceback):
         end_traceback = end_traceback.tb_next
     if issubclass(type_, NameError):
         assert len(value.args) == 1
+        error_msg = value.args[0]
         if issubclass(type_, UnboundLocalError):
-            match = re.match("^local variable '(\w+)' referenced before assignment$", value.args[0])
+            match = re.match("^local variable '(\w+)' referenced before assignment$", error_msg)
         else:
-            match = re.match("^(?:global )?name '(\w+)' is not defined$", value.args[0])
-        assert match, "No match for %s" % value.args[0]
+            match = re.match("^(?:global )?name '(\w+)' is not defined$", error_msg)
+        assert match, "No match for %s" % error_msg
         var, = match.groups()
-        sugg = get_var_suggestions(var, end_traceback.tb_frame)
-        value.args = (value.args[0] + get_suggestion_string(sugg), )
+        sugg = get_name_suggestions(var, end_traceback.tb_frame)
+        value.args = (error_msg + get_suggestion_string(sugg), )
         assert len(value.args) == 1
     elif issubclass(type_, AttributeError):
         assert len(value.args) == 1
-        match = re.match("^'?(\w+)'? (?:object|instance) has no attribute '(\w+)'$", value.args[0])
-        assert match, "No match for %s" % value.args[0]
-        type_str, method = match.groups()
-        sugg = get_method_suggestions(type_str, method, end_traceback.tb_frame)
-        value.args = (value.args[0] + get_suggestion_string(sugg), )
+        error_msg = value.args[0]
+        match = re.match("^'?(\w+)'? (?:object|instance) has no attribute '(\w+)'$", error_msg)
+        assert match, "No match for %s" % error_msg
+        type_str, attr = match.groups()
+        sugg = get_attribute_suggestions(type_str, attr, end_traceback.tb_frame)
+        value.args = (error_msg + get_suggestion_string(sugg), )
         assert len(value.args) == 1
     elif issubclass(type_, TypeError):
         assert len(value.args) == 1
-        match = re.match("^'(\w+)' object (?:is (?:not |un)subscriptable|has no attribute '__getitem__')$", value.args[0])
+        error_msg = value.args[0]
+        match = re.match("^'(\w+)' object (?:is (?:not |un)subscriptable|has no attribute '__getitem__')$", error_msg)
         if match:  # It could be cool to extract relevant info from the trace
             type_str, = match.groups()
             if type_str == 'function':
-                value.args = (value.args[0] + get_suggestion_string([type_str + '(value)']), )
+                value.args = (error_msg + get_suggestion_string([type_str + '(value)']), )
         assert len(value.args) == 1
     elif issubclass(type_, ImportError):
         assert len(value.args) == 1
-        match = re.match("^No module named '?(\w+)'?$", value.args[0])
+        error_msg = value.args[0]
+        match = re.match("^No module named '?(\w+)'?$", error_msg)
         if match:
             module_str, = match.groups()
-            sugg = get_close_matches(module_str, STAND_MODULES)
-            value.args = (value.args[0] + get_suggestion_string(sugg), )
+            sugg = get_module_name_suggestion(module_str)
+            value.args = (error_msg + get_suggestion_string(sugg), )
         else:
-            match = re.match("^cannot import name '?(\w+)'?$", value.args[0])
-            assert match, "No match for %s" % value.args[0]
+            match = re.match("^cannot import name '?(\w+)'?$", error_msg)
+            assert match, "No match for %s" % error_msg
             imported_fail, = match.groups()
             sugg = get_imported_suggestion(imported_fail, end_traceback.tb_frame)
-            value.args = (value.args[0] + get_suggestion_string(sugg), )
+            value.args = (error_msg + get_suggestion_string(sugg), )
         assert len(value.args) == 1
     else:
         print(type_, value.args)
