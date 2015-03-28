bd4b6b7f81ae9ea5c322b3bad39035614df8fd8a
Sylvain <sylvain.desodt+github@gmail.com>
2015-03-28 11:30:58 +0000

    Fix style

--- didyoumean/didyoumean.py
+++ didyoumean/didyoumean.py
@@ -23,7 +23,8 @@ ATTRIBUTEERROR_RE = r"^'?([\w\.]+)'? (?:object|instance) " \
     "has no attribute '(\w+)'$"
 UNSUBSCRIBTABLE_RE = r"^'(\w+)' object " \
     "(?:is (?:not |un)subscriptable|has no attribute '__getitem__')$"
-UNEXPECTED_KEYWOORDARG_RE = r"(\w+)\(\) got an unexpected keyword argument '(\w+)'"
+UNEXPECTED_KEYWOORDARG_RE = r"(\w+)\(\) " \
+    "got an unexpected keyword argument '(\w+)'"
 NOMODULE_RE = r"^No module named '?(\w+)'?$"
 CANNOTIMPORT_RE = r"^cannot import name '?(\w+)'?$"
 
