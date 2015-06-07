0074b3de0ef2daa4fe3469f94ba53e595c261876
Sylvain <sylvain.desodt+github@gmail.com>
2015-06-07 22:56:07 +0100

    Re-trying to play wih Pypi but still no luck
    The file organisation and/or the import might be wrong. When I try to
    use the code after installing, I get :
    
    >>> import didyoumean.didyoumean_hook
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
      File "/home/stephanie/.sylvain/env3.4/lib/python3.4/site-packages/didyoumean/didyoumean_hook.py", line 3, in <module>
        from didyoumean import add_suggestions_to_exception
    ImportError: cannot import name 'add_suggestions_to_exception'
    
    To be investigated... but not today.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+try:
+        from setuptools import setup
+except ImportError:
+        from distutils.core import setup
 # with the help from http://peterdowns.com/posts/first-time-with-pypi.html
+# http://www.scotttorborg.com/python-packaging/minimal.html
 setup(
-    name='didyoumean',
-    packages=['didyoumean'],  # this must be the same as the name above
-    version='0.1',
+    name='didyoumean-suggestions',
+    packages=['didyoumean'],
+    version='0.2',
     description=('Logic to have suggestions in case of errors '
-                 '(NameError, AttributeError, etc).'),
+                 '(NameError, AttributeError, ImportError, TypeError, etc).'),
     author='Sylvain Desodt',
     author_email='sylvain.desodt+didyoumean@gmail.com',
     url='https://github.com/SylvainDe/DidYouMean-Python',
@@ -16,6 +20,8 @@ setup(
         'error',
         'suggestion',
         'excepthook',
+        'decorator',
+        'contextmanager',
         'typo'],
     classifiers=[],
 )
