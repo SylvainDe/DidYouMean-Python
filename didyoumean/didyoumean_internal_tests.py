164f5205c4084f42b5a3538068b92efc4b145bb7
Sylvain <sylvain.desodt+github@gmail.com>
2015-08-22 11:07:06 +0100

    Run generated tests on Python 3.6

--- didyoumean/didyoumean_internal_tests.py
+++ didyoumean/didyoumean_internal_tests.py
@@ -594,6 +594,7 @@ def generate_add_string_to_exc_tests():
                     (AddStringToExcFromInstanceTest, unittest2.TestCase),
                     {'exc_type': klass, 'args': p})
 
+generate_add_string_to_exc_tests()
 
 if __name__ == '__main__':
     print(sys.version_info)
