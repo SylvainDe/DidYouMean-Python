dbeec56d01ec46e8bcab5be6a8c7353c0a6d0c8b
SylvainDe <sylvain.desodt+github@gmail.com>
2019-09-08 01:22:21 +0200

    Fix error in previous commit
    Code
    
     print >> 1, 5
    
    did not work on Python 2:
    
     AttributeError("'int' object has no attribute 'write'",)

--- didyoumean/didyoumean_sugg_tests.py
+++ didyoumean/didyoumean_sugg_tests.py
@@ -1765,7 +1765,10 @@ class TypeErrorTests(GetSuggestionsTests):
         # Inspired from PyPy commit:
         # https://bitbucket.org/pypy/pypy/commits/c0b2526268ab96a0b3caa7bdafa2cf0309a73a20
         before, after = before_and_after((3, 0))
-        for code in ["print >> 1, 5", "print -1"]:
+        for code in [
+            "f = open('/tmp/foo', 'w')\nprint >> f, 5",
+            "print -1",
+        ]:
             self.runs(code, before)
             self.throws(code, UNSUPPORTEDOPERAND, [], after)
 
