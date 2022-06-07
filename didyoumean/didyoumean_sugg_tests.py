65e08f26a613f5ffd2675c82f4c16cc50cd05e57
SylvainDe <sylvain.desodt+github@gmail.com>
2022-06-07 16:00:03 +0200

    Even more cleanup to make test case minimal

--- didyoumean/didyoumean_sugg_tests.py
+++ didyoumean/didyoumean_sugg_tests.py
@@ -25,26 +25,10 @@ def endlessly_recursive_func(n):
     return endlessly_recursive_func(n - 1)
 
 
-def listify(value, default, expected_types):
-    """Return list from value, using default value if value is None."""
-    if value is None:
-        value = list(default)
-    if not isinstance(value, list):
-        value = [value]
-    if default:
-        assert all(v in default for v in value), "%s %s" % (value, default)
-    if expected_types is not None:
-        assert all(isinstance(v, expected_types) for v in value), "%s %s" % (
-            value,
-            expected_types,
-        )
-    return value
-
-
 def get_exception(code):
     """Helper function to run code and get what it throws (or None)."""
     try:
-        exec(code)
+        code(0)
     except:
         return sys.exc_info()
     return None
@@ -53,24 +37,11 @@ def get_exception(code):
 class GetSuggestionsTests(unittest_module.TestCase):
     """Generic class to test things."""
 
-    def throws(self, code, error_type, error_msg):
+    def throws(self, code, error_msg):
         """Run code and check it throws."""
-        details = "Running following code :\n---\n{0}\n---".format(code)
-
-        exc = get_exception(code)
-
-        self.assertFalse(exc is None, "No exc thrown." + details)
-        type_caught, value, traceback = exc
-
-        self.assertTrue(isinstance(value, type_caught))
-        self.assertTrue(
-            issubclass(type_caught, error_type),
-            "{0} ({1}) not a subclass of {2}".format(type_caught, value, error_type)
-            + details,
-        )
-
+        _, value, _ = get_exception(code)
         msg = next((a for a in value.args if isinstance(a, str)), "")
-        self.assertRegexp(msg, error_msg, details)
+        self.assertRegexp(msg, error_msg)
 
     def assertRegexp(self, text, regex, msg=None):
         """Wrapper around the different names for assertRegexp...."""
@@ -86,9 +57,7 @@ class RuntimeErrorTests(GetSuggestionsTests):
     def test_max_depth(self):
         """Reach maximum recursion depth."""
         sys.setrecursionlimit(200)
-        code = "endlessly_recursive_func(0)"
-        max_recur_regexp = r"^maximum recursion depth exceeded$"
-        self.throws(code, RuntimeError, max_recur_regexp)
+        self.throws(endlessly_recursive_func, r"^maximum recursion depth exceeded$")
         sys.setrecursionlimit(initial_recursion_limit)
 
 
