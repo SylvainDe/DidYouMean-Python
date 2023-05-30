dc6c0a24c0a4058b401a2ed9453f90e4acb3ae5c
SylvainDe <sylvain.desodt+github@gmail.com>
2023-05-30 19:55:49 +0200

    Test issue W391 on Pycodestyle (on Python 3.12.0b1)

--- didyoumean/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Empty file. Might grow in the future."""
-import didyoumean_api
---
+++ setup2.py
@@ -0,0 +1,30 @@
+"""Setup."""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+# with the help from http://peterdowns.com/posts/first-time-with-pypi.html
+# http://www.scotttorborg.com/python-packaging/minimal.html
+setup(
+    name='BetterErrorMessages',
+    packages=['didyoumean'],
+    version='0.4',
+    description=('Logic to have suggestions in case of errors '
+                 '(NameError, AttributeError, ImportError, TypeError, etc).'),
+    author='Sylvain Desodt',
+    author_email='sylvain.desodt+didyoumean@gmail.com',
+    url='https://github.com/SylvainDe/DidYouMean-Python',
+    download_url='https://github.com/SylvainDe/DidYouMean-Python/tarball/0.1',
+    keywords=[
+        'didyoumean',
+        'exception',
+        'error',
+        'suggestion',
+        'excepthook',
+        'decorator',
+        'contextmanager',
+        'typo'],
+    classifiers=[],
+)
+
